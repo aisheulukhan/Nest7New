7244220812ac49f56ba8296f4315e8975baa19dc
aisheulukhan <tu7hnsxs1@code.edu.az>
2022-06-01 18:07:33 +0400

    add

---
+++ Nest5/Nest3/TaskNest/TaskNest/ViewModels/HomeVM.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TaskNest.Models;
+
+namespace TaskNest.ViewModels
+{
+    public class HomeVM
+    {
+        public List<Carusel> Carusels { get; set; }
+        public List<Category> Categories { get; set; }
+        public List<Product> Products { get; set; }
+        public List<Product> RecentProducts { get; set; }
+        public List<Product> TopRatedProducts { get; set; }
+    }
+}
